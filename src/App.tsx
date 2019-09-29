c7f0c770e10a6e338cb0af2f31f419980798dd7c
Xavyr Moss <xavyr.moss@cybergrx.com>
2019-09-29 15:31:52 -0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import tony from "./tony.jpg";
+import "./App.css";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={tony} className="App-logo" alt="tony" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn huh
+        </a>
+      </header>
+    </div>
+  );
+};
+
+export default App;
