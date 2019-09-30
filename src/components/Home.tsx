7ae7654af9018fcb20afc234c6193d967b00f701
Xavyr Moss <xavyr.moss@cybergrx.com>
2019-09-29 19:56:25 -0700

    link

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,29 +1,29 @@
 import * as React from "react";
 import lightbulbs from "../images/lightbulbs.svg";
+import { Route, BrowserRouter, Link } from "react-router-dom";
+import { Titans } from "./Titans";
 import "../App.css";
 
 export const Home: React.FC = ({}) => {
   return (
     <div
       style={{
-        display: "flex",
-        flexDirection: "column",
-        height: "100%",
-        width: "100%",
         alignItems: "center"
       }}
     >
-      <img
-        src={lightbulbs}
-        alt="lightbulbs-logo"
-        style={{
-          width: "100%",
-          height: "auto",
-          maxHeight: 700,
-          margin: "0 auto",
-          alignItems: "center"
-        }}
-      />
+      <Link to={`/Titans`}>
+        <img
+          src={lightbulbs}
+          alt="lightbulbs-logo"
+          style={{
+            width: "100%",
+            height: 700,
+            // maxHeight: 700,
+            margin: "0 auto",
+            alignItems: "center"
+          }}
+        />
+      </Link>
       <h1
         style={{
           width: "100%",
@@ -36,80 +36,7 @@ export const Home: React.FC = ({}) => {
       >
         Break the block and <br /> find your inspiration.
       </h1>
-      <div>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-around",
-            marginTop: 100,
-            marginBottom: 10,
-            marginLeft: 10,
-            marginRight: 10
-          }}
-        >
-          <button
-            style={{
-              backgroundColor: "#979797",
-              color: "white",
-              borderRadius: 10,
-              padding: 20,
-              fontFamily: "Roboto",
-              fontSize: "17px"
-            }}
-          >
-            Home
-          </button>
-          <button
-            style={{
-              backgroundColor: "#979797",
-              color: "white",
-              borderRadius: 10,
-              padding: 20,
-              fontFamily: "Roboto",
-              fontSize: "17px"
-            }}
-          >
-            About
-          </button>
-          <button
-            style={{
-              backgroundColor: "#979797",
-              color: "white",
-              borderRadius: 10,
-              padding: 20,
-              fontFamily: "Roboto",
-              fontSize: "17px"
-            }}
-          >
-            Titans
-          </button>
-          <button
-            style={{
-              backgroundColor: "#979797",
-              color: "white",
-              borderRadius: 10,
-              padding: 20,
-              fontFamily: "Roboto",
-              fontSize: "17px"
-            }}
-          >
-            Data Visualizations
-          </button>
-          <button
-            style={{
-              backgroundColor: "#979797",
-              color: "white",
-              borderRadius: 10,
-              padding: 20,
-              fontFamily: "Roboto",
-              fontSize: "17px"
-            }}
-          >
-            Data Entry
-          </button>
-        </div>
-      </div>
+      <div></div>
     </div>
   );
 };
